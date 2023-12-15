ebd58b9e3b37e006e00cb5ab20ed76f62afc7c62
jon albert virrey <113243693+Jonvry@users.noreply.github.com>
2023-12-15 16:23:34 +0800

    add port

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -2,10 +2,12 @@ import mysql, { ConnectionOptions } from "mysql2/promise";
 
 export const db = (async () => {
   const access: ConnectionOptions = {
+    port: Number(process.env.MYSQL_PORT), // Parse the port as a number
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
+    connectionLimit: 100,
   };
 
   const Connection = await mysql.createConnection(access);
