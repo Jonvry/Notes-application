08c2c9451160db91dc072bd08786bb93ec09904a
jon albert virrey <113243693+Jonvry@users.noreply.github.com>
2023-12-16 14:34:52 +0800

    fix the connection

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -5,8 +5,8 @@ export const db = (async () => {
     port: Number(process.env.MYSQL_PORT), // Parse the port as a number
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
-    password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQL_DATABASE,
+    password: process.env.MYSQL_PASSWORD,
   };
 
   const Connection = await mysql.createConnection(access);
